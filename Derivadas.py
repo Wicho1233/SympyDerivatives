f24772371d66a2469bf305a76a0f4b6a4125053c
Luis Alberto Castro Cruz <castroluisalberto984@gmail.com>
2025-02-12 23:22:26 -0600

    Agregue comentarios

--- Derivadas.py
+++ Derivadas.py
@@ -1,5 +1,5 @@
-import sympy as sym
-x= sym.Symbol('x')
-func = input('Ingrese la funcion: \n')
-result =sym.Derivative(func,x,evaluate =True)
-print(result)
+import sympy as sym # importar librerias
+x= sym.Symbol('x')#Declarar variable
+func = input('Ingrese la funcion: \n')#Pedir fumcion
+result =sym.Derivative(func,x,evaluate =True)#Derivar la funcion
+print(result)# Mostrar resultado
