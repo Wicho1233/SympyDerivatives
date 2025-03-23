0ae0291159850800df6eb58b337558323dee9030
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-03-22 22:51:27 -0600

    Update Derivadas.py

--- Derivadas.py
+++ Derivadas.py
@@ -1,6 +1,7 @@
 import sympy as sym # importar librerias
 #Declarar variable
 x= sym.Symbol('x')
-func = input('Ingrese la funcion: \n')#Pedir fumcion
+#Pedir fumcion
+func = input('Ingrese la funcion: \n')
 result =sym.Derivative(func,x,evaluate =True)#Derivar la funcion
 print(result)# Mostrar resultado
